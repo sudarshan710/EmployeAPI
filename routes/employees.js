634b9bcea8b8c963b8115f88a0211faea65eea7b
Sudarshan Zunja <100218588+sudarshan710@users.noreply.github.com>
2023-04-25 19:09:44 +0530

    put-delete-added

--- routes/employees.js
+++ routes/employees.js
@@ -21,16 +21,47 @@ router.get('/:id', async (req, res) =>{
     }
 });
 
-router.post('/', async (req, res) =>{
+router.post('/add/', async (req, res) =>{
+    const dataRec = new Data({
+        id: req.body.id,
+        name: req.body.name,
+        department: req.body.department,            
+        age: req.body.age
+    })
 
+    try {
+        console.log('Here');
+        const dataSaved = await dataRec.save();
+        res.status(200).json(dataSaved);
+    } catch(error){
+        res.status(400).json({message: error.message});
+    }
 });
 
-router.put('/:id', (req, res) =>{
-    res.send('Hello!');
+router.put('/update/:id', async (req, res) =>{
+    try {
+        const dataID = req.params.id;
+        const newData = req.body;
+        const options =  { new: true };
+
+        const result = await Data.findByIdAndUpdate(dataID, newData, options);
+        
+        res.send(result);
+
+    } catch(error){
+        res.status(400).json({ message: error.message});
+    }
 });
 
-router.delete('/:id', (req, res) =>{
-    res.send('Hello!');
+router.delete('/delete/:id', async (req, res) =>{
+    try {
+        const dataID = req.params.id;
+        const result = await Data.findByIdAndDelete(dataID);
+        res.send(`Document with ${result.name} has been deleted!`);
+
+    } catch(error){
+        res.status(400).json({ message: error.message});
+    }
 });
 
 export default router;
